4bf6184d9afa30b1039f4c4e168d9254c76f54ff
WLHHYYYY <williamhidayat_2226250035@mhs.mdp.ac.id>
2025-09-18 20:31:11 +0700

    fixxx

---
+++ untitled2/Pertemuan1/Latihan2.kt
@@ -0,0 +1,18 @@
+fun main() {
+    //membuat variable menggunakan type variable yang tepat
+    val idProduct : Long = 9876543210L
+    val hargaProduk : Double = 3499990.50
+    val beratProdukKg: Float = 0.15F
+    val ujumlahStok: UInt = 250u
+    val jumlahTerjual : Int = 1843
+    val ukodeKategori : UByte = 12u
+    val tahunRilis : Int = 2025
+
+    println("Id Produk          = $idProduct")
+    println("Harga Produk       = $hargaProduk")
+    println("Berat Produk (KG)  = $beratProdukKg Kg")
+    println("Jumlah Stok        = $ujumlahStok")
+    println("Jumlah Terjual     = $jumlahTerjual")
+    println("Kode Kategori      = $ukodeKategori")
+    println("Tahun Rilis        = $tahunRilis")
+}
---
+++ untitled2/Pertemuan1/Variable.kt
@@ -0,0 +1,19 @@
+const val APP_NAME = "Belajar Kotlin" //Diletakkan diluar function
+const val APP_VERSION = "0.0.1"
+
+fun main() {
+    var playerScore = 100 //gunakan var jika nilainya berubah-ubah
+    println(playerScore)
+
+    //----------------------------------------------------------//
+    playerScore = 103
+    println(playerScore)
+
+    //----------------------------------------------------------//
+
+    val npm = "2226250064" //selalu gunakan val jika nilainya tetap
+    //npm = "2226250064"
+    println(npm)
+
+    println(APP_NAME)
+}
