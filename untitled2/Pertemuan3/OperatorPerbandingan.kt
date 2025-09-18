cd59392783fbc54081985bc75d1d922df26bb1cf
WLHHYYYY <williamhidayat_2226250035@mhs.mdp.ac.id>
2025-09-18 20:40:44 +0700

    Pertemuan 3

---
+++ untitled2/Pertemuan3/OperatorPerbandingan.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val nilaiA = 20
+    val nilaiB = 15
+
+    val apakahLebihBesar = nilaiA > nilaiB
+    println(apakahLebihBesar)
+
+    val apakahSama = nilaiA == nilaiB
+    println(apakahSama)
+
+    val apakahTidakSama = nilaiA != nilaiB
+    println(apakahTidakSama)
+    
+}
